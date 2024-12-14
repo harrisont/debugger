0cc6c164638b1a9a170ae35c7a7dfe57e6e893ff
Harrison Ting <hting@riotgames.com>
2024-12-14 09:47:43 -0800

    Style refactor

--- src/eval.rs
+++ src/eval.rs
@@ -8,11 +8,12 @@ pub struct EvalContext<'a> {
     pub process: &'a mut Process,
 }
 
-// TODO: Expression evaluation needs an evaluation context. Possibly includnig memory read, register read, and symbol names.
 pub fn evaluate_expression(expr: EvalExpr, context: &mut EvalContext) -> Result<u64, String> {
     match expr {
         EvalExpr::Number(x) => Ok(x),
-        EvalExpr::Add(x, _, y) => Ok(evaluate_expression(*x, context)? + evaluate_expression(*y, context)?),
+        EvalExpr::Add(x, _, y) => {
+            Ok(evaluate_expression(*x, context)? + evaluate_expression(*y, context)?)
+        }
         EvalExpr::Symbol(symbol) => {
             resolve_name_to_address(&symbol, context.process)
         }
